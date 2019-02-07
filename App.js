e9a6a1ad84dd24f18babfc757ccd3f62a01fdd60
Aubrey Wiedeman <intetra@gmail.com>
2019-02-07 01:19:24 +0000

    back up

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Header } from './src/components/common';
+import reducers from './src/reducers';
+import LibraryList from './src/components/LibraryList';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText='Tech Stack' />
+        <LibraryList />
       </View>
-    );
-  }
-}
+    </Provider>
+  );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/components/common/Header.js
@@ -0,0 +1,40 @@
+// import libraries for making a Component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// make a Component
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+//define styling
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    position: 'relative',
+    //shadow settings
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    // elevation gets shadows working on android
+    elevation: 3,
+    //zIndex gets shadows working on iOS
+    zIndex: 999
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+// make the component available to other parts of the app
+export { Header };
